e50fc474a9a1d554f5f7279401242907ece42116
Alessandro Panico <142506840+alessandropanico@users.noreply.github.com>
2024-06-26 15:18:16 +0200

    foto e immagini migliorate

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+//per poter fare foto anche da pc
 import { defineCustomElements } from '@ionic/pwa-elements/loader';
 
 defineCustomElements(window);
