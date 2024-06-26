2936a6309edc46513d58b1c8d9fa2652ff4ad5c7
Alessandro Panico <142506840+alessandropanico@users.noreply.github.com>
2024-06-26 14:52:12 +0200

    sa

--- src/app/pagine/camera/camera.component.ts
+++ src/app/pagine/camera/camera.component.ts
@@ -7,7 +7,9 @@ import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
   styleUrls: ['./camera.component.scss'],
 })
 export class CameraComponent {
-  photo: string | undefined;
+  photos: string[] = []; // Array per memorizzare le foto
+  currentPage = 1; // Pagina corrente
+  itemsPerPage = 15; // Numero di foto per pagina
 
   constructor() { }
 
@@ -18,16 +20,49 @@ export class CameraComponent {
         quality: 90,
         allowEditing: false,
         resultType: CameraResultType.DataUrl,
-        source: CameraSource.Camera, // Utilizza la fotocamera
+        source: CameraSource.Camera,
       });
 
       console.log('Photo taken:', image);
 
-      this.photo = image.dataUrl;
+      // Aggiungi la foto all'array di foto
+      this.photos.push(image.dataUrl ?? '');
+
     } catch (error) {
       console.error('Error taking photo', error);
     }
   }
 
+  // Calcola il numero totale di pagine
+  totalPages(): number {
+    return Math.ceil(this.photos.length / this.itemsPerPage);
+  }
 
+  // Restituisce l'array di foto per la pagina corrente
+  getCurrentPagePhotos(): string[] {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    return this.photos.slice(startIndex, startIndex + this.itemsPerPage);
+  }
+
+  // Restituisce un array di numeri per la paginazione
+  paginationRange(): number[] {
+    const total = this.totalPages();
+    return Array.from({ length: total }, (_, index) => index + 1);
+  }
+
+  // Cambia la pagina corrente
+  changePage(page: number) {
+    this.currentPage = page;
+  }
+
+  // Restituisce le righe di foto per la pagina corrente
+  getCurrentPageRows(): string[][] {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const pagePhotos = this.photos.slice(startIndex, startIndex + this.itemsPerPage);
+    const rows: string[][] = [];
+    for (let i = 0; i < pagePhotos.length; i += 5) {
+      rows.push(pagePhotos.slice(i, i + 5));
+    }
+    return rows;
+  }
 }
