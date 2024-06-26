5477f544df8b9c12dbd99b69fa3c49cc217a93ae
Alessandro Panico <142506840+alessandropanico@users.noreply.github.com>
2024-06-26 12:04:00 +0200

    android studio installato

--- src/app/pagine/camera/camera.component.ts
+++ src/app/pagine/camera/camera.component.ts
@@ -1,14 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 
 @Component({
   selector: 'app-camera',
   templateUrl: './camera.component.html',
   styleUrls: ['./camera.component.scss'],
 })
-export class CameraComponent  implements OnInit {
+export class CameraComponent {
+  photo: string | undefined;
 
   constructor() { }
 
-  ngOnInit() {}
+  async takePhoto() {
+    console.log('Taking photo...');
+    try {
+      const image = await Camera.getPhoto({
+        quality: 90,
+        allowEditing: false,
+        resultType: CameraResultType.DataUrl,
+        source: CameraSource.Camera,
+      });
+
+      console.log('Photo taken:', image);
+
+      this.photo = image.dataUrl;
+    } catch (error) {
+      console.error('Error taking photo', error);
+    }
+  }
 
 }
