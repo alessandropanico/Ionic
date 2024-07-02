6f5c5142b5ae7510c68ecfd106521436251dbbbe
Alessandro Panico <142506840+alessandropanico@users.noreply.github.com>
2024-07-02 15:33:41 +0200

    ingrandimento foto

--- src/app/pagine/camera/camera.component.ts
+++ src/app/pagine/camera/camera.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { Geolocation } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-camera',
@@ -7,10 +8,9 @@ import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
   styleUrls: ['./camera.component.scss'],
 })
 export class CameraComponent {
-  photos: string[] = []; // Array per memorizzare le foto
-  currentPage = 1; // Pagina corrente
-  itemsPerPage = 15; // Numero di foto per pagina
-
+  photos: { src: string, lat?: number, lng?: number }[] = [];
+  currentPage = 1;
+  itemsPerPage = 15;
   zoomedPhoto: string | null = null;
 
   constructor() { }
@@ -19,81 +19,76 @@ export class CameraComponent {
     console.log('Taking photo...');
     try {
       const image = await Camera.getPhoto({
-        quality: 100, // Imposta la qualità al massimo
+        quality: 100,
         allowEditing: false,
         resultType: CameraResultType.DataUrl,
         source: CameraSource.Camera,
-        saveToGallery: true, // Opzionale: salva la foto nella galleria
+        saveToGallery: true,
       });
 
       console.log('Photo taken:', image);
 
-      // Aggiungi la foto all'array di foto
-      this.photos.push(image.dataUrl ?? '');
+      const position = await Geolocation.getCurrentPosition();
+      console.log('Current position:', position);
+
+      this.photos.push({
+        src: image.dataUrl ?? '',
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      });
 
     } catch (error) {
-      console.error('Error taking photo', error);
+      console.error('Error taking photo or getting location', error);
 
-      // Gestisci il caso in cui l'utente abbia negato i permessi
       if (error === 'Permission denied') {
         console.error('Permission denied by user');
       }
     }
   }
 
-  // Metodo per eliminare una foto
-  deletePhoto(photo: string) {
+  deletePhoto(photo: { src: string, lat?: number, lng?: number }) {
     this.photos = this.photos.filter(p => p !== photo);
   }
 
-  // Calcola il numero totale di pagine
   totalPages(): number {
     return Math.ceil(this.photos.length / this.itemsPerPage);
   }
 
-  // Restituisce l'array di foto per la pagina corrente
-  getCurrentPagePhotos(): string[] {
+  getCurrentPagePhotos(): { src: string, lat?: number, lng?: number }[] {
     const startIndex = (this.currentPage - 1) * this.itemsPerPage;
     return this.photos.slice(startIndex, startIndex + this.itemsPerPage);
   }
 
-  // Restituisce un array di numeri per la paginazione
   paginationRange(): number[] {
     const total = this.totalPages();
     return Array.from({ length: total }, (_, index) => index + 1);
   }
 
-   // Cambia la pagina corrente
-   changePage(page: number) {
+  changePage(page: number) {
     this.currentPage = page;
   }
 
-  // Vai alla pagina successiva
   nextPage() {
     if (this.currentPage < this.totalPages()) {
       this.currentPage++;
     }
   }
 
-  // Vai alla pagina precedente
   previousPage() {
     if (this.currentPage > 1) {
       this.currentPage--;
     }
   }
 
-  // Restituisce le righe di foto per la pagina corrente
-  getCurrentPageRows(): string[][] {
+  getCurrentPageRows(): { src: string, lat?: number, lng?: number }[][] {
     const startIndex = (this.currentPage - 1) * this.itemsPerPage;
     const pagePhotos = this.photos.slice(startIndex, startIndex + this.itemsPerPage);
-    const rows: string[][] = [];
+    const rows: { src: string, lat?: number, lng?: number }[][] = [];
     for (let i = 0; i < pagePhotos.length; i += 5) {
       rows.push(pagePhotos.slice(i, i + 5));
     }
     return rows;
   }
-
-  //------------------------------
 
   zoomPhoto(photo: string) {
     this.zoomedPhoto = photo;
